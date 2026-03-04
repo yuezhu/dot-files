67f22cfe4da1439b93b454ab64e6d40d37a69710
Yue Zhu <1224535+yuezhu@users.noreply.github.com>
2026-03-04 07:39:20 -0500

    emacs: update completions

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -442,13 +442,13 @@ Usage:
 
 
 (use-package electric
-  :hook
-  (after-init . electric-indent-mode))
+  :init
+  (electric-indent-mode))
 
 
 (use-package elec-pair
-  :hook
-  (after-init . electric-pair-mode))
+  :init
+  (electric-pair-mode))
 
 
 (use-package ediff-wind
@@ -490,7 +490,6 @@ Usage:
   :bind ("C-c u" . transient-winner-undo)
 
   :hook
-  ;;  (after-init . winner-mode)
   (ediff-before-setup . winner-mode)
   (ediff-quit . winner-undo)
 
@@ -586,8 +585,7 @@ Usage:
 
 (use-package ispell
   :defer t
-  ;; 2023-08-12 use `flyspell-correct'
-  ;;  :bind ("C-c i w" . ispell-word)
+
   :custom
   (ispell-program-name "hunspell")
   (ispell-personal-dictionary "~/.emacs.d/ispell-personal-dictionary")
@@ -597,6 +595,7 @@ Usage:
       "[[:alpha:]]" "[^[:alpha:]]" "[']" nil
       ("-d" "en_US") nil utf-8)))
   (ispell-local-dictionary "en_US")
+
   :config
   ;; Hunspell cannot create the personal dictionary file if it does not exist.
   (unless (file-exists-p ispell-personal-dictionary)
@@ -604,12 +603,9 @@ Usage:
 
 
 (use-package flyspell
-  :bind ("C-c i b" . flyspell-buffer)
-  :hook
-  (text-mode . flyspell-mode)
-  :custom
-  (flyspell-sort-corrections t)
-  :config
+  :bind ("C-c s b" . flyspell-buffer)
+
+  :preface
   ;; https://github.com/abo-abo/oremacs/blob/github/modes/ora-flyspell.el
   (defun flyspell-ignore-http-and-https ()
     "Function used for `flyspell-generic-check-word-predicate' to
@@ -617,6 +613,12 @@ ignore stuff starting with \"http\" or \"https\"."
     (save-excursion
       (forward-whitespace -1)
       (not (looking-at "[\t ]+https?\\b"))))
+
+  :hook
+  (text-mode . flyspell-mode)
+  :custom
+  (flyspell-sort-corrections t)
+  :config
   (dolist (mode '(text-mode org-mode)) ;; need to specify all derived modes
     (put mode 'flyspell-mode-predicate #'flyspell-ignore-http-and-https)))
 
@@ -625,7 +627,7 @@ ignore stuff starting with \"http\" or \"https\"."
   :ensure t
   :after flyspell
   :bind (:map flyspell-mode-map
-              ("C-c i w" . flyspell-correct-wrapper)))
+              ("C-c s w" . flyspell-correct-wrapper)))
 
 
 (use-package flymake
@@ -954,15 +956,20 @@ this is effective with some expand functions, eg.,
 
 (use-package vertico
   :ensure t
-  ;; :hook (after-init . vertico-mode)
   :init
   (vertico-mode)
+
   :custom
+  ;; (vertico-scroll-margin 0) ;; Different scroll margin
+  (vertico-count 20) ;; Show more candidates
+  ;; (vertico-resize t) ;; Grow and shrink the Vertico minibuffer
+  ;; (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
   (vertico-multiform-commands
    '((consult-yank-pop indexed)
      (consult-yank-replace indexed)
      (consult-imenu (completion-ignore-case . t))
      (consult-recent-file (completion-ignore-case . t))))
+
   :config
   (vertico-multiform-mode))
 
@@ -970,53 +977,125 @@ this is effective with some expand functions, eg.,
 (use-package consult
   :ensure t
   :after vertico
+
   ;; Replace bindings. Lazily loaded by `use-package'.
   :bind (;; C-c bindings in `mode-specific-map'
          ("C-c M-x" . consult-mode-command)
-         ("C-c TAB" . consult-imenu)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         ("C-c i" . consult-info)
          ([remap Info-search] . consult-info)
-
          ;; C-x bindings in `ctl-x-map'
-         ("C-x b"   . consult-buffer)
-         ("C-x 4 b" . consult-buffer-other-window)
-         ("C-x 5 b" . consult-buffer-other-frame)
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
          ("C-x C-r" . consult-recent-file)
-         ("C-x p b" . consult-project-buffer)
-         ("C-x p g" . consult-ripgrep)
-
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
          ;; Other custom bindings
-         ("M-y"     . consult-yank-replace)
-         ("C-M-s"   . consult-line)
-
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; ("M-y" . consult-yank-replace)
          ;; M-g bindings in `goto-map'
-         ("M-g M-g" . consult-goto-line)
-         ("M-g i"   . consult-imenu)
-
+         ("M-g e" . consult-compile-error)
+         ("M-g r" . consult-grep-match)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+         ("M-s c" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
          ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
          :map isearch-mode-map
-         ("M-o" . consult-line))
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
 
+  ;; The :init configuration is always executed (Not lazy)
   :init
+
+  ;; Tweak the register preview for `consult-register-load',
+  ;; `consult-register-store' and the built-in commands.  This improves the
+  ;; register formatting, adds thin separator lines, register sorting and hides
+  ;; the window mode line.
+  (advice-add #'register-preview :override #'consult-register-window)
+  (setq register-preview-delay 0.5)
+
   ;; Use Consult to select xref locations with preview
   (setq xref-show-xrefs-function #'consult-xref
         xref-show-definitions-function #'consult-xref)
 
-  ;; Trigger preview manually
-  (setq consult-preview-key "M-.")
-
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
   :config
+
   ;; Explicitly require `recentf' in order to use `consult-recent-file'.
   (require 'recentf)
-  (setq completion-in-region-function #'consult-completion-in-region))
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  (setq consult-preview-key "M-.")
+  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  ;; (consult-customize
+  ;;  consult-theme :preview-key '(:debounce 0.2 any)
+  ;;  consult-ripgrep consult-git-grep consult-grep consult-man
+  ;;  consult-bookmark consult-recent-file consult-xref
+  ;;  consult-source-bookmark consult-source-file-register
+  ;;  consult-source-recent-file consult-source-project-recent-file
+  ;;  ;; :preview-key "M-."
+  ;;  :preview-key '(:debounce 0.4 any))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; "C-+"
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (keymap-set consult-narrow-map (concat consult-narrow-key " ?") #'consult-narrow-help)
+  )
 
 
+;; Enable rich annotations using the Marginalia package
 (use-package marginalia
   :ensure t
   :after vertico
+
+  ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
+  ;; available in the *Completions* buffer, add it to the
+  ;; `completion-list-mode-map'.
   :bind (:map minibuffer-local-map
               ("M-A" . marginalia-cycle))
 
+  ;; The :init section is always executed.
   :init
+
   ;; Marginalia must be activated in the :init section of use-package such that
   ;; the mode gets enabled right away. Note that this forces loading the
   ;; package.
@@ -1053,47 +1132,21 @@ this is effective with some expand functions, eg.,
 
 (use-package embark-consult
   :ensure t ; only need to install it, embark loads it after consult if found
-  :after (embark consult)
-  :hook
-  (embark-collect-mode . consult-preview-at-point-mode))
+  :after (embark consult))
 
 
 (use-package corfu
   :ensure t
   :custom
-
-  ;; Only use `corfu' when calling `completion-at-point' or
-  ;; `indent-for-tab-command'
   (corfu-auto nil)
-  (corfu-auto-prefix 2)
-  (corfu-auto-delay 0.25)
-
   (corfu-min-width 80)
   (corfu-max-width corfu-min-width)     ; Always have the same width
   (corfu-count 14)
   (corfu-scroll-margin 4)
-  (corfu-cycle nil)
+  (corfu-cycle nil)                ;; Enable cycling for `corfu-next/previous'
 
-  ;; `nil' means to ignore `corfu-separator' behavior, that is, use the older
-  ;; `corfu-quit-at-boundary' = nil behavior. Set this to separator if using
-  ;; `corfu-auto' = `t' workflow (in that case, make sure you also set up
-  ;; `corfu-separator' and a keybind for `corfu-insert-separator', which my
-  ;; configuration already has pre-prepared). Necessary for manual corfu usage with
-  ;; orderless, otherwise first component is ignored, unless `corfu-separator'
-  ;; is inserted.
-  (corfu-quit-at-boundary nil)
-  (corfu-separator ?\s)            ; Use space
-  (corfu-quit-no-match 'separator) ; Don't quit if there is `corfu-separator' inserted
-  (corfu-preview-current 'insert)  ; Preview first candidate. Insert on input if only one
-  (corfu-preselect-first t)        ; Preselect first candidate?
-
-  :bind (:map corfu-map
-              ("<escape>" . corfu-quit)
-              ("M-l"      . corfu-info-location)
-              ("M-d"      . corfu-popupinfo-toggle))
-
-  :hook
-  (after-init . global-corfu-mode))
+  :init
+  (global-corfu-mode))
 
 
 (use-package corfu-popupinfo
@@ -1113,10 +1166,25 @@ this is effective with some expand functions, eg.,
 (use-package cape
   :ensure t
   :after corfu
+  ;; Bind prefix keymap providing all Cape commands under a mnemonic key.
+  ;; Press C-c p ? to for help.
+  :bind ("C-c p" . cape-prefix-map) ;; Alternative key: M-<tab>, M-p, M-+
+  ;; Alternatively bind Cape commands individually.
+  ;; :bind (("C-c p d" . cape-dabbrev)
+  ;;        ("C-c p h" . cape-history)
+  ;;        ("C-c p f" . cape-file)
+  ;;        ...)
   :init
-  (add-to-list 'completion-at-point-functions #'cape-keyword t)
-  (add-to-list 'completion-at-point-functions #'cape-dabbrev t)
-  (add-to-list 'completion-at-point-functions #'cape-file t))
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  (add-hook 'completion-at-point-functions #'cape-dabbrev)
+  (add-hook 'completion-at-point-functions #'cape-file)
+  (add-hook 'completion-at-point-functions #'cape-elisp-block)
+  ;; (add-hook 'completion-at-point-functions #'cape-history)
+  ;; ...
+  )
 
 
 (use-package copilot
